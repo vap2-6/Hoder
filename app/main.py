8d9d09fb3218bd7e524221e93881b27fea790e17
vap2-6 <akashpillai006@gmail.com>
2026-02-11 11:04:10 +0530

    Update main.py

--- app/main.py
+++ app/main.py
@@ -1,1 +1,12 @@
+from fastapi import FastAPI
+from .routes import auth, user, admin
 
+app = FastAPI(title="Secure Cloud Backend")
+
+app.include_router(auth.router, prefix="/auth", tags=["Authentication"])
+app.include_router(user.router, prefix="/user", tags=["User"])
+app.include_router(admin.router, prefix="/admin", tags=["Admin"])
+
+@app.get("/")
+def root():
+    return {"status": "Secure Backend Running"}
