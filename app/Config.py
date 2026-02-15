cc0fb3a30d776d396f7044031cb77e65d7aa3550
vap2-6 <akashpillai006@gmail.com>
2026-02-15 17:22:59 +0530

    Update import statement for BaseSettings

--- app/Config.py
+++ app/Config.py
@@ -1,12 +1,16 @@
-from pydantic import BaseSettings
+from pydantic_settings import BaseSettings
+
 
 class Settings(BaseSettings):
     DATABASE_URL: str
     SECRET_KEY: str
     ALGORITHM: str = "HS256"
     ACCESS_TOKEN_EXPIRE_MINUTES: int = 30
+    REFRESH_TOKEN_EXPIRE_DAYS: int = 7
 
     class Config:
         env_file = ".env"
+        case_sensitive = True
+
 
 settings = Settings()
