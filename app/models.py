bda799c94b4dd255d5c0f5e6cb1a1f59a1bbf890
vap2-6 <akashpillai006@gmail.com>
2026-02-15 16:31:07 +0530

    Make email and hashed_password fields non-nullable

--- app/models.py
+++ app/models.py
@@ -5,6 +5,6 @@ class User(Base):
     __tablename__ = "users"
 
     id = Column(Integer, primary_key=True, index=True)
-    email = Column(String, unique=True, index=True)
-    hashed_password = Column(String)
+    email = Column(String, unique=True, index=True, nullable=False)
+    hashed_password = Column(String, nullable=False)
     role = Column(String, default="user")
